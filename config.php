e01c73086f01791cc12071a737cb06ccf24dd947
Akande Lateef <Akandelateef0@gmail.com>
2023-09-09 02:23:33 +0100

    Update config.php
    Added content type to to header

--- config.php
+++ config.php
@@ -3,4 +3,5 @@
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Credentials: true");
 header("Access-Control-Allow-Methods: GET");
+header("Content-Type: application/json");
 ?>
